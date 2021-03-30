e27c82296c644e36e34f96a83e46fef1af57d8b4
ThomasHinterberger <[Thomas.hinterberger@gmail.com]>
2021-03-31 01:54:43 +0200

    return JSON now

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,28 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use serde::{*};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-struct Info {
-    username: String,
-}
-
-async fn index(info: web::Json<Info>) -> impl Responder {
-    format!("Welcome {}!", info.username)
-}
-
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-            
+            .route("/", web::get().to(greet))
+            .route("/hey", web::get().to(current_temperature))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+#[derive(Serialize)]
+struct Measurement {
+    temperature: f32,
+}
+
+async fn current_temperature() -> impl Responder {
+    web::Json(Measurement { temperature: 42.3 })
 }
